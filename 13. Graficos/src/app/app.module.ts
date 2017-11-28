ebeaa8c5a577812c147d935e3cf9cda5cff99847
edgon <edgon85@gmail.com>
2017-11-28 13:33:52 -0600

    finalizada la seccion de graficos

---
+++ 13. Graficos/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//fragicos
+import { ChartsModule } from 'ng2-charts';
+
+import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LineaComponent,
+    BarraComponent
+  ],
+  imports: [
+    BrowserModule,
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
